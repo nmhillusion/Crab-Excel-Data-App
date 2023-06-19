b0ac7b8959b5c55c8a0b089b97e72a33f8113dd3
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-06-19 20:59:45 +0700

    add test case for string validator

---
+++ Crab-Excel-Data-App-Test/Validator/StringValidatorTest.cs
@@ -0,0 +1,24 @@
+﻿using CrabExcelDataApp.Validator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Crab_Excel_Data_App_Test.Validator
+{
+    [TestClass]
+    public class StringValidatorTest
+    {
+        [TestMethod]
+        public void TestIsBlank()
+        {
+            Assert.IsFalse(StringValidator.IsBlank("a"));
+            Assert.IsFalse(StringValidator.IsBlank("d "));
+            Assert.IsFalse(StringValidator.IsBlank("c  "));
+            Assert.IsFalse(StringValidator.IsBlank("  b"));
+            Assert.IsFalse(StringValidator.IsBlank("  e "));
+
+            Assert.IsTrue(StringValidator.IsBlank(null));
+            Assert.IsTrue(StringValidator.IsBlank(""));
+            Assert.IsTrue(StringValidator.IsBlank(" "));
+            Assert.IsTrue(StringValidator.IsBlank("                     "));
+        }
+    }
+}
