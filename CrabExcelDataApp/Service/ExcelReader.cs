2ca7a038484b0b70f302210c0a2509054681fb2d
nmhillusion <nguyenminhhieu.geek@gmail.com>
2023-06-21 22:14:03 +0700

    impl excel reader with ignore hidden rows option

--- CrabExcelDataApp/Service/ExcelReader.cs
+++ CrabExcelDataApp/Service/ExcelReader.cs
@@ -1,11 +1,13 @@
 ﻿using CrabExcelDataApp.Model;
 using CrabExcelDataApp.Service.Logger;
 using ExcelDataReader;
+using Microsoft.Office.Interop.Excel;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace CrabExcelDataApp.Service
 {
@@ -20,6 +22,23 @@ namespace CrabExcelDataApp.Service
         }
 
         public List<TableModel> ReadData(string excelFilePath)
+        {
+            return ReadData(excelFilePath, true);
+        }
+
+        public List<TableModel> ReadData(string excelFilePath, bool isIgnoreHiddenRows)
+        {
+            if (!isIgnoreHiddenRows)
+            {
+                return ReadDataAllRows(excelFilePath);
+            }
+            else
+            {
+                return ReadDataAndIgnoreHiddenRows(excelFilePath);
+            }
+        }
+
+        public List<TableModel> ReadDataAllRows(string excelFilePath)
         {
             logHelper.Info("Read Excel at " + excelFilePath);
             try
@@ -47,7 +66,7 @@ namespace CrabExcelDataApp.Service
 
                 for (int tableIdx = 0; tableIdx < dataTables.Count; ++tableIdx)
                 {
-                    DataTable dataTable = dataTables[tableIdx];
+                    System.Data.DataTable dataTable = dataTables[tableIdx];
                     DataRowCollection dataRowCollection = dataTable.Rows;
 
                     logHelper.Debug("Table Name: " + dataTable.TableName);
@@ -79,6 +98,84 @@ namespace CrabExcelDataApp.Service
             {
                 logHelper.Error(ex);
                 return new List<TableModel>();
+            }
+        }
+
+        public List<TableModel> ReadDataAndIgnoreHiddenRows(string excelFilePath)
+        {
+            logHelper.Info("Read Excel at " + excelFilePath);
+
+            Application excelApp = null;
+            Workbook workbook = null;
+
+            try
+            {
+                excelApp = new Application();
+                workbook = excelApp.Workbooks.Open(excelFilePath);
+
+                List<TableModel> totalData = new List<TableModel>();
+
+                int sheetCount = workbook.Worksheets.Count;
+                logHelper.Info($" sheetCount: {sheetCount}");
+
+
+                foreach (Worksheet worksheet in workbook.Worksheets)
+                {
+                    Range firstCell = worksheet.Cells[1, 1];
+                    Range lastCellInColumn = firstCell.End[XlDirection.xlDown];
+                    Range lastCellInRow = firstCell.End[XlDirection.xlToRight];
+
+                    int lastRow = lastCellInColumn.Row;
+                    int lastColumn = lastCellInRow.Column;
+                    logHelper.Info($" lastRow: {lastRow}");
+                    logHelper.Info($" lastColumn: {lastColumn}");
+
+                    List<List<object>> sheetData = new List<List<object>>();
+
+                    for (int rowNum = 1; rowNum <= lastRow; ++rowNum)
+                    {
+                        Range row_ = worksheet.Rows[rowNum];
+
+                        if (!row_.Hidden)
+                        {
+                            List<object> rowData = new List<object>();
+                            for (int colNum = 1; colNum <= lastColumn; ++colNum)
+                            {
+                                Range cell_ = worksheet.Cells[rowNum, colNum];
+                                rowData.Add(cell_.Value);
+                            }
+                            sheetData.Add(rowData);
+                        }
+                    }
+
+                    TableModel model = new TableModel()
+                    {
+                        tableName = worksheet.Name
+                    };
+                    model.SetTableData(sheetData);
+                    totalData.Add(model);
+                }
+
+                return totalData;
+            }
+            catch (Exception ex)
+            {
+                logHelper.Error(ex);
+                return new List<TableModel>();
+            }
+            finally
+            {
+                if (null != workbook)
+                {
+                    workbook.Close();
+                    Marshal.ReleaseComObject(workbook);
+                }
+
+                if (null != excelApp)
+                {
+                    excelApp.Quit();
+                    Marshal.ReleaseComObject(excelApp);
+                }
             }
         }
     }
